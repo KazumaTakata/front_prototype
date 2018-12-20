09fa2aaaed1a1accc4101077cc2187f95d33dfa1
Kazuma Takata <neilbuzztakata@gmail.com>
2018-12-20 18:12:32 +0900

    f

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
+"use strict";
 
-'use strict';
+var gulp = require("gulp");
+var sass = require("gulp-sass");
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
-gulp.task('sass', function () {
-  return gulp.src('./scss/index.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+gulp.task("sass", function() {
+  return gulp
+    .src("./scss/index.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css"));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./scss/*.scss', ['sass']);
+gulp.task("sass:watch", function() {
+  gulp.watch("./scss/*.scss", ["sass"]);
 });
 
-
-gulp.task('default', ['sass:watch']);
+gulp.task("default", ["sass:watch"]);
